d8cec7cc64bb9c3b18234e0422db9648a8125989
Willian <williansilvap25@gmail.com>
2023-02-02 15:51:02 -0300

    add axios

--- src/components/Aside/Aside.jsx
+++ src/components/Aside/Aside.jsx
@@ -1,18 +1,41 @@
+import { useState } from "react";
+import { api } from "../../services/Api";
 import { AsideStyled } from "./AsideStyled";
 
 export default function Aside() {
+    const [title, setTitle] = useState('');
+    const [notes, setNotes] = useState('');
+
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const response = await api.post("/notes",
+            title,
+            notes
+        )
+        setTitle("");
+        setNotes("");
+    }
+
     return (
         <>
             <AsideStyled>
                 <strong>Caderno de Notas</strong>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-block">
                         <label htmlFor="title">Título da Anotação</label>
-                        <input />
+                        <input
+                            required
+                            value={title}
+                            onChange={e => setTitle(e.target.value)}
+                        />
                     </div>
                     <div className="input-block">
-                        <label htmlFor="nota">Anotações</label>
-                        <textarea></textarea>
+                        <label htmlFor="notes">Anotações</label>
+                        <textarea
+                            required
+                            value={notes}
+                            onChange={e => setNotes(e.target.value)}
+                        />
                     </div>
                     <button type="submit">Salvar</button>
                 </form>
